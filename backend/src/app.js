40cc68b393be8bcf24d113cf75a63b89a14e8149
joilsonmslopes <joilsonmirandasl@hotmail.com>
2020-03-29 20:44:32 -0300

    aplicação mobile

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes'); // importando uma rota
 
 const app = express();
@@ -7,5 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json()); //Usado antes das rotas para tornar o objeto json entendível pela aplicação
 app.use(routes);
+app.use(errors());
 
-app.listen(3333)
+module.exports = app;
--- backend/src/controller/OngController.js
+++ backend/src/controller/OngController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto'); //Para retornar uma encriptação de algum ID ou Senha
+const generateUniqueId = require('../utils/generateUniqueId'); //Para retornar uma encriptação de algum ID ou Senha
 const connection = require('../database/connection');
 
 module.exports = {
@@ -11,7 +11,7 @@ module.exports = {
     async create(request, response) {
         const { name, email, whatsapp, city, uf } = request.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
     
         await connection('ongs').insert({
             id,
